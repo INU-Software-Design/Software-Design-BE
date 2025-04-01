8d3f759ea2adc64f6f994379dc33184727a270d6
82everywin <82everywin@gmail.com>
2025-04-01 09:45:01 +0900

    env: 환경셋팅

---
+++ src/main/java/com/neeis/neeis/NeeisApplication.java
@@ -0,0 +1,13 @@
+package com.neeis.neeis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeeisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeeisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neeis/neeis/NeeisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neeis.neeis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeeisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
