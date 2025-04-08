e6f9fc5386dc513fb9a4233150fe8af14cb6d0a6
82everywin <82everywin@gmail.com>
2025-04-08 11:29:11 +0900

    feat: userDetails 추가

---
+++ src/main/java/com/neeis/neeis/global/jwt/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.neeis.neeis.global.jwt;
+
+import com.neeis.neeis.domain.user.User;
+import com.neeis.neeis.domain.user.UserRepository;
+import com.neeis.neeis.global.exception.CustomException;
+import com.neeis.neeis.global.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow( () -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        log.info("UserDetailsService: {}", user.getUsername());
+        return new CustomUserDetails(user);
+    }
+}
