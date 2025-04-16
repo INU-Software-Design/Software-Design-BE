79a1652e0985be566f350d2b5571b56d354daff3
82everywin <82everywin@gmail.com>
2025-04-17 02:01:35 +0900

    feat: 담당 학급 및 학생 조회시 query 작성

--- src/main/java/com/neeis/neeis/domain/classroom/ClassroomRepository.java
+++ src/main/java/com/neeis/neeis/domain/classroom/ClassroomRepository.java
@@ -1,11 +1,9 @@
 package com.neeis.neeis.domain.classroom;
 
-import com.neeis.neeis.domain.student.Student;
-import com.neeis.neeis.domain.teacher.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+
 import java.util.Optional;
 
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
@@ -13,4 +11,8 @@ public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
     @Query(value = "select * from classroom " +
             "where teacher_id = :teacherId and year = :year;", nativeQuery = true)
     Optional<Classroom> findByTeacherIdAndYear(Long teacherId, int year);
+
+    @Query(value = "select * from classroom " +
+            "where year = :year and grade = :grade and class_num = :classNum and teacher_id = :teacherId;", nativeQuery = true)
+    Optional<Classroom> findByClassroomInfo(int year, int grade, int classNum, Long teacherId);
 }
