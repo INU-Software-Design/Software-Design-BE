a3fbb2b7109dd1338e529aff2a6badd099fe8182
82everywin <82everywin@gmail.com>
2025-05-02 19:21:46 +0900

    docs: swagger 문서 설명 추가

--- src/main/java/com/neeis/neeis/domain/subject/dto/req/CreateSubjectRequestDto.java
+++ src/main/java/com/neeis/neeis/domain/subject/dto/req/CreateSubjectRequestDto.java
@@ -1,6 +1,8 @@
 package com.neeis.neeis.domain.subject.dto.req;
 
 import com.neeis.neeis.domain.subject.Subject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CreateSubjectRequestDto {
+
+    @NotBlank(message = "과목 명은 필수 입력값입니다.")
+    @Schema(description = "과목 명" , example = "영어")
     private String name;
 
     @Builder
