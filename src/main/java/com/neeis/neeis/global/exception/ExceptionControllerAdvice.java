e9988900cce2c62f325cc764bc36522eefdd2a05
82everywin <82everywin@gmail.com>
2025-05-06 18:50:54 +0900

    fix: json 입력 잘못 들어올 시 에러 처리

--- src/main/java/com/neeis/neeis/global/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/neeis/neeis/global/exception/ExceptionControllerAdvice.java
@@ -3,6 +3,7 @@ package com.neeis.neeis.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -39,5 +40,12 @@ public class ExceptionControllerAdvice {
         log.error("handleException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getCode()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleJsonParseError(HttpMessageNotReadableException e) {
+        log.warn("handleJsonParseError", e);
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_JSON);
+        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
     }
 }
